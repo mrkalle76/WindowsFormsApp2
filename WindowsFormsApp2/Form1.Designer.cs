7736d620237e03422f55155a4e2a5a8a1f0985f1
Karl Johan Lennart Petersson <mrkalle76@hotmail.com>
2017-12-09 11:49:42 +0100

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.LBoxDic = new System.Windows.Forms.ListBox();
+            this.programBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(215, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(853, 551);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(25, 82);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(184, 467);
+            this.treeView.TabIndex = 12;
+            this.treeView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.treeView_MouseDoubleClick);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(25, 555);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(184, 41);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "Updatera Filmlista";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LBoxDic
+            // 
+            this.LBoxDic.FormattingEnabled = true;
+            this.LBoxDic.Items.AddRange(new object[] {
+            "K:/",
+            "J:/"});
+            this.LBoxDic.Location = new System.Drawing.Point(25, 45);
+            this.LBoxDic.Name = "LBoxDic";
+            this.LBoxDic.Size = new System.Drawing.Size(184, 17);
+            this.LBoxDic.TabIndex = 15;
+            // 
+            // programBindingSource
+            // 
+            this.programBindingSource.DataSource = typeof(WindowsFormsApp2.Program);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1099, 609);
+            this.Controls.Add(this.LBoxDic);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.treeView);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.programBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource programBindingSource;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.ListBox LBoxDic;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+
+        string substringDirectory;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            treeView.Nodes.Clear();
+            String path = "K:\\";
+
+            treeView.Nodes.Add(path);
+            PopulateTreeView(path, treeView.Nodes[0], 1);
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void datagridAddMovie(string filePath, string movietitle)
+        {
+            string[] row = new string[] { filePath, movietitle, filePath };
+            dataGridView1.Rows.Add(row);
+            dataGridView1.Refresh();
+
+        }
+
+        private string GetMKVTitle(string filePath)
+        {
+            bool bTagFound = false;
+            
+            Debug.WriteLine("GetMKVTitle: " + filePath);
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "C:\\MediaInfo.exe",
+                    Arguments = "\"" + filePath + "\"",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            proc.Start();
+
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                string line = proc.StandardOutput.ReadLine();
+                Match match = Regex.Match(line, @"Movie name\s+:(.+)$");
+                if (match.Success)
+                {
+                    bTagFound = true;
+                    Debug.WriteLine(line);
+                    return match.Groups[1].Value.Trim();
+                }
+            }
+
+            if (!bTagFound)
+            {
+                Debug.WriteLine("ERROR: COULD NOT GET TITLE: " + filePath);
+                updateMKVFile(filePath, "N/A");
+                return "N/A";
+            }
+            else { return null; }
+        }
+
+        private void InitializeDataGrid()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+
+            dataGridView1.ColumnCount = 3;
+            dataGridView1.Columns[0].Name = "Filename";
+            dataGridView1.Columns[1].Name = "Title Property";
+            dataGridView1.Columns[2].Name = "FilenameSaved";
+            dataGridView1.Columns[2].Visible = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        }
+
+        private void listfiles(string Dpath)
+        {
+            InitializeDataGrid();
+
+            DirectoryInfo DirInfo = new DirectoryInfo(Dpath);
+
+            foreach (FileInfo fi in DirInfo.GetFiles("*.mkv"))
+            {
+                string MovieTitle = GetMKVTitle(fi.FullName);
+                if (MovieTitle != null)
+                {
+                    datagridAddMovie(fi.FullName, MovieTitle);
+                }
+
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void PopulateTreeView(string directoryValue, TreeNode parentNode, int level)
+        {
+            Debug.WriteLine("PopulateTreeView: " + directoryValue + " " + level);
+
+
+            string[] directoryArray = Directory.GetDirectories(directoryValue);
+
+            try
+            {
+                if (directoryArray.Length != 0)
+                {
+                    foreach (string directory in directoryArray)
+                    {
+                        substringDirectory = directory.Substring(directory.LastIndexOf('\\') + 1);
+                        if (substringDirectory.Contains("System Volume Information") || substringDirectory.Contains("$RECYCLE.BIN"))
+                        {
+                            Debug.WriteLine("Skipped " + directoryValue);
+                        }
+                        else
+                        { 
+                            Debug.WriteLine(substringDirectory);
+                            TreeNode myNode = new TreeNode(substringDirectory);
+                            parentNode.Nodes.Add(myNode);
+                            PopulateTreeView(directory, myNode, level+=1);
+                            dataGridView1.Refresh();
+
+                        }
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                parentNode.Nodes.Add("Access denied");
+            } // end catch
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show(treeView.SelectedNode.FullPath.ToString());
+            listfiles(treeView.SelectedNode.FullPath.ToString());
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 1)
+            {
+                updateMKVFile(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString(), dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString());
+            }
+            else if (e.ColumnIndex == 0)
+            {
+                Debug.WriteLine("{0} renamed to {1}", dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString(), dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+                Debug.WriteLine(dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString());
+                File.Move(dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString(), dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+                dataGridView1.Rows[e.RowIndex].Cells[2].Value = dataGridView1.Rows[e.RowIndex].Cells[0].Value;
+
+            }
+        }
+
+        private void updateMKVFile(string filePath, string newTitle)
+        {
+            Debug.WriteLine("updateMKVFile " + filePath + " " + newTitle);
+            string sArg = "\"" + filePath + "\" --edit info --set \"title=" + newTitle + "\"";
+            Debug.WriteLine(sArg);
+
+
+            if (true)
+            {
+                var proc = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "C:\\Program Files (x86)\\MKVtoolnix\\mkvpropedit.exe",
+                        //"The Magnificent Seven (2016).mkv" --edit info --set "title=The Magnificent Seven (2016)"
+                        Arguments = sArg,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+
+                proc.Start();
+
+                while (!proc.StandardOutput.EndOfStream)
+                {
+                    string line = proc.StandardOutput.ReadLine();
+                    Debug.WriteLine(line);
+                    
+                }
+
+
+            }
+
+
+        }
+
+        private void treeView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (treeView.SelectedNode != null)
+            {
+                listfiles(treeView.SelectedNode.FullPath.ToString());
+            }
+
+
+        }
+    }
+}
